b03aacf6a92563b8ca414646b8da64baab615bf5
mickaelchoidev <mickaelchoidev@gmail.com>
2020-07-02 00:02:20 +0200

    project set up, react-router-dom imported, working on home page + navigation bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>Chairs</li>
+        <li>Sofas</li>
+        <li>Tables</li>
+        <li>Lamps</li>
+        <li>Kit</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
